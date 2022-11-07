4120a54fa1cc8a475d6f01711ee84114cffa63fa
Tanya <tanyagoyal247@gmail.com>
2022-11-07 10:40:13 +0530

    somecode

--- Form1.cs
+++ Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
@@ -15,11 +16,28 @@ namespace paintApp
         public Form1()
         {
             InitializeComponent();
-        }
 
+            this.Width = 900;
+            this.Height = 700;
+            bm = new Bitmap(pic.Width, pic.Height);
+            g = Graphics.FromImage(bm);
+            g.Clear(Color.White);
+        }
+        Bitmap bm;
+        Graphics g;
+        bool paint = false;
+        Point px, py;
+        Pen p = new Pen(Color.Black, 1);
+        int index;
+        int x, y, sY, sX, Cx, Cy;
+
+        private void pic_MouseDown(object sender, MouseEventArgs e)
+        {
+            paint = true;
+        }
         private void pic_Paint(object sender, PaintEventArgs e)
         {
-
+            
         }
     }
 }
